22c0fb5bc5ddd23f2193419c81b6d1697fab533e
Microsoft Tech Club LPS <45787667+MTCLPS@users.noreply.github.com>
2018-12-11 11:50:29 +0100

    Add files via upload

---
+++ Assets/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public float speed = 30;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.name == "Player1")
+        {
+            float dis = (transform.position.y - col.transform.position.y) / col.collider.bounds.size.y;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(1, dis) * speed;
+        }
+        if (col.gameObject.name == "Player2")
+        {
+            float dis = (transform.position.y - col.transform.position.y) / col.collider.bounds.size.y;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-1, dis) * speed;
+        }
+    }
+
+}
---
+++ Assets/Player1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+    public string i;
+    public float speed = 30;
+	// Use this for initialization
+	void Update () {
+        float v = Input.GetAxisRaw(i) * speed;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v);
+	}
+	
+	// Update is called once per frame
+}
